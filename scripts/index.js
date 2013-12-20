9eceac80688c08dda1da6a1d8e26d672f81df440
rohanbhangui <bhanguirohan@gmail.com>
2013-12-19 23:56:52 -0800

    Revert "<select> dropdown attempt 1"
    This reverts commit a24ec7fda12637f024293a400fe2dd84554bcded.

--- scripts/index.js
+++ scripts/index.js
@@ -1,24 +1,5 @@
 $(document).ready(function () {
 
-	var $selectElem = $("select");
-	$selectElem.find("option").map(function() {
-			var $this = $(this);
-			return $("<li>").attr("value", $this.attr("value")).text($this.text()).get();
-	}).appendTo($("<ul>").attr({
-			id: $selectElem.attr("id"),
-			name: $selectElem.attr("name"),
-			class: "select"
-	})).parent().replaceAll($selectElem);
-
-	$(".select > li:not(:first-child)").hide();
-
-	$(".select").prepend($(".select > li:first-child"));
-
-	$(document).on("click", function(event) {
-		var target = $(event.target).parent();
-
-		console.log($(event.target).parent());
-	});
-
+  
 
 });
